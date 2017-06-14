8367981d140308780e50cd37027dc83ba40d9390
Vatekeh Corlon <vatekehcorlon@Vatekehs-MacBook-Pro.local>
2017-06-14 14:03:56 -0500

    all

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = 3001;
+var app = express();
+
+var messages = [];
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+app.use(function(req, res, next){
+    console.log('this is middlware')
+    next()
+})
+
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages/chat', function(req, res, next) {
+  res.send("Got here first")
+})
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
+
+
+
+
+app.listen(port, function(){
+    console.log('hw')
+});
